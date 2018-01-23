01dc6bf25539901fa8531e0566d4f6611e7373ec
mayurkhatri <mayurkhatri122@gmail.com>
2018-01-23 14:33:00 +0530

    Unused code removed

--- src/App.js
+++ src/App.js
@@ -3,19 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e){
+  divClicked(e){
     e.preventDefault();
-    console.log("button clicked");
-  }
-
-  testEvent(e){
-    e.preventDefault();
-    console.log("div clicked");
     document.getElementById("displayDiv").innerHTML = e.target.innerHTML + " clicked";
   }
 
@@ -24,11 +13,11 @@ class App extends Component {
       <div className="App">
         <h1>Please click on any text message</h1>
         <br/>
-        <div onClick={this.testEvent}>First message</div>
-        <div onClick={this.testEvent}>Second message</div>
-        <div onClick={this.testEvent}>Third message</div>
-        <div onClick={this.testEvent}>Fourth message</div>
-        <div onClick={this.testEvent}>Hey there</div>
+        <div onClick={this.divClicked}>First message</div>
+        <div onClick={this.divClicked}>Second message</div>
+        <div onClick={this.divClicked}>Third message</div>
+        <div onClick={this.divClicked}>Fourth message</div>
+        <div onClick={this.divClicked}>Hey there</div>
         <br/>
         <div id="displayDiv"></div>
       </div>
