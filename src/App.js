0cf9fdc651d5aac0e73756a013bed627086f4bb0
mayurkhatri <mayurkhatri122@gmail.com>
2018-01-23 15:51:28 +0530

    Multiple components, use of props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class PrintMessage extends Component {
   divClicked(e){
     e.preventDefault();
     document.getElementById("displayDiv").innerHTML = e.target.innerHTML + " clicked";
   }
+  render(){
+    return(
+      <div onClick={this.divClicked}>{this.props.message}</div>
+    );
+  }
+}
 
+class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Please click on any text message</h1>
         <br/>
-        <div onClick={this.divClicked}>First message</div>
-        <div onClick={this.divClicked}>Second message</div>
-        <div onClick={this.divClicked}>Third message</div>
-        <div onClick={this.divClicked}>Fourth message</div>
-        <div onClick={this.divClicked}>Hey there</div>
+        <PrintMessage message="Here we go" />
+        <PrintMessage message="First message" />
+        <PrintMessage message="Second message" />
+        <PrintMessage message="Third message" />
+        <PrintMessage message="Fourth message" />
+        <PrintMessage message="Hey there" />
         <br/>
         <div id="displayDiv"></div>
       </div>
